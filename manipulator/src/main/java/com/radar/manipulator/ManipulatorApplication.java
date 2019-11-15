072481f4edd6cfeaecde066ec64d411b6283be69
Jonathan Davis <cartman61616@gmail.com>
2019-11-15 14:57:19 -0500

    Adding API project scaffold

---
+++ manipulator/src/main/java/com/radar/manipulator/ManipulatorApplication.java
@@ -0,0 +1,13 @@
+package com.radar.manipulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManipulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManipulatorApplication.class, args);
+	}
+
+}
---
+++ manipulator/src/test/java/com/radar/manipulator/ManipulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radar.manipulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManipulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
